0695dae2bd51878f8e717011aca00312f5616318
marianaliima <marilimabrito13@gmail.com>
2024-06-14 10:21:37 -0300

    refactor: change mocking strategy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,14 +18,7 @@ return new EmailValidatorStub()
 
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error();
-    }
-  }
-  return new EmailValidatorStub()
-}
+
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
@@ -125,15 +118,16 @@ describe("SignUp Controller", () => {
     expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 
+  
   test("Should return 500 if EmailValidator throws", () => {
-   
-    const emailValidatorStub = makeEmailValidatorWithError();
-    const sut = new SignUpController(emailValidatorStub);
-
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "isValid").mockImplementationOnce(() => {
+            throw new Error()
+    });
     const httpRequest = {
       body: {
         name: "any_name",
-        email: "any_email@mail.com",
+        email: "invalid_email@mail.com",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
