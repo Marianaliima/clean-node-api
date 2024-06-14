099cbbe9e9bf3f730ee8a98ef5c99f4d27914f5a
marianaliima <marilimabrito13@gmail.com>
2024-06-14 11:25:04 -0300

    feat: ensure SingUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -136,4 +136,20 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test("Should return 400 if password confirmation fails", () => {
+    const { sut} = makeSut();
+    
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "invalid_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "invalid_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("passwordConfirmation"));
+  });
 });
